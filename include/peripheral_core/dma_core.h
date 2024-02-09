96f2a847b4d911bf6032edb6363920ad096ba9ca
Aaron425 <151467777+AaronMcBrideUW@users.noreply.github.com>
2024-02-09 13:10:16 -0800

    SYNC (ignore)

--- include/peripheral_core/dma_core.h
+++ include/peripheral_core/dma_core.h
@@ -17,8 +17,6 @@
 //// SECTION: DMA VARS & DEFS
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-#define _nocpy_(_name_) private: _name_(){}
-
 DmacDescriptor wbDescArray[DMAC_CH_NUM] __attribute__ ((aligned (16)));
 DmacDescriptor baseDescArray[DMAC_CH_NUM] __attribute__ ((aligned (16))); 
 
@@ -42,101 +40,97 @@ enum DMACH_ERROR {
   DMACH_ERROR_TRANSFER
 };
 
-////////////// DMA DEFAULT CONFIGS /////////////
+typedef void (*callbackFunction)(void) dmaErrorCallback;
 
-#define DMA_DEFAULT_PRILVL 
-
+#define IRQ_MAX_PRIORITY 4 // FIND SOME WAY TO GET THIS ACTUALLY
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //// SECTION: DMA FUNCTIONS
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 struct {
-  bool priority_lvl_enabled[DMA_PRILVL_COUNT] = { true };
+  struct {
+    bool enabled = false;
+    bool roundRobinMode = false;
+    int serviceQuality = 2;
+  }priorityLvl[DMA_PRILVL_COUNT];
+
   uint8_t irq_priority_lvl = 1;
-  uint8_t service_quality = 0;
-}dma_config;
+}config;
 
-struct { 
- 
-  struct { 
 
-    /// @brief Enables/disables the dma system.
-    /// @param bool -> Set true to reset & enable dma, or false 
-    ///                reset & disable it.
-    /// @return bool -> True if dma is enabled, false otherwise.
-    struct { 
-      inline operator bool() {
-        return static_cast<bool>(DMAC->CTRL.bit.DMAENABLE);
+typedef struct dma_ctrl_enabled { 
+  inline operator bool() {
+    return static_cast<bool>(DMAC->CTRL.bit.DMAENABLE);
+  }
+  inline bool operator = (const bool value) {
+    DMAC->CTRL.bit.DMAENABLE = 0;
+    DMAC->CRCCTRL.reg &= ~DMAC_CRCCTRL_MASK;
+    DMAC->CTRL.bit.SWRST = 1;
+    while(DMAC->CTRL.bit.SWRST);
+    if (value) {
+      if (config.priorityLvl->serviceQuality > 3
+        ||config.irq_priority_lvl > IRQ_MAX_PRIORITY) {
+        return false;
       }
-      inline bool operator = (const bool value) {
-        DMAC->CTRL.bit.DMAENABLE = 0;
-        DMAC->CRCCTRL.reg &= ~DMAC_CRCCTRL_MASK;
-        DMAC->CTRL.bit.SWRST = 1;
-        while(DMAC->CTRL.bit.SWRST);
-        if (value) {
-          for (int i = 0; i < DMA_IRQ_COUNT; i++) {
-            NVIC_ClearPendingIRQ((IRQn_Type)(DMAC_0_IRQn + i));
-            NVIC_SetPriority((IRQn_Type)(DMAC_0_IRQn + i), 
-              dma_config.irq_priority_lvl);
-            NVIC_EnableIRQ((IRQn_Type)(DMAC_0_IRQn + i));
-          }
-          for (int i = 0; i < DMA_PRILVL_COUNT; i++) {
-            DMAC->CTRL.reg |= (dma_config.priority_lvl_enabled[i] 
-              << DMAC_CTRL_LVLEN0_Pos + i);            
-          }
-          DMAC->BASEADDR.reg = (uintptr_t)baseDescArray;
-          DMAC->WRBADDR.reg = (uintptr_t)wbDescArray;
-          // DMAC->PRICTRL0.bit
-        }
-        return true;
+      for (int i = 0; i < DMA_IRQ_COUNT; i++) {
+        NVIC_ClearPendingIRQ((IRQn_Type)(DMAC_0_IRQn + i));
+        NVIC_SetPriority((IRQn_Type)(DMAC_0_IRQn + i), 
+          config.irq_priority_lvl);
+        NVIC_EnableIRQ((IRQn_Type)(DMAC_0_IRQn + i));
       }
-    }enabled;
-
-
-
-  }sys;
-
-  struct {
-
-
-  }channel;
-
-  struct {
-
-  }crc;
-
-  struct {
-
-  }utils;
-
-  struct {
-   
-    struct {
-      inline operator bool() { 
-        return DMAC->ACTIVE.bit.ABUSY; 
+      for (int i = 0; i < DMA_PRILVL_COUNT; i++) {
+        DMAC->CTRL.reg |= 
+          ((uint8_t)config.priorityLvl[i].enabled 
+            << (DMAC_CTRL_LVLEN0_Pos + i));    
+        DMAC->PRICTRL0.reg |= 
+          ((uint8_t)config.priorityLvl[i].roundRobinMode 
+            << (DMAC_PRICTRL0_RRLVLEN0_Pos + i * 8))
+        | ((uint8_t)config.priorityLvl[i].serviceQuality 
+            << (DMAC_PRICTRL0_QOS0_Pos + i * 8));
       }
-    }busy;
+      DMAC->BASEADDR.reg = (uint32_t)baseDescArray;
+      DMAC->WRBADDR.reg = (uint32_t)wbDescArray;
+      DMAC->CTRL.bit.DMAENABLE = 1;
+    }
+    return true;
+  }
+};
 
-    struct {
-      inline operator int() { 
-        return DMAC->ACTIVE.bit.ID; 
-      }
-    }number;
+typedef struct dma_active_remainingBytes{
+  operator int() {
+    uint32_t count = DMAC->ACTIVE.bit.BTCNT;
+    if (DMAC->ACTIVE.bit.ABUSY || !count) {
+      return 0;
+    } else 
+    return count / wbDescArray[DMAC->ACTIVE.bit.ID].BTCTRL.bit.BEATSIZE;
+  }
+};
 
-    struct {
-      operator int() {
-        uint32_t count = DMAC->ACTIVE.bit.BTCNT;
-        if (DMAC->ACTIVE.bit.ABUSY || !count) {
-          return 0;
-        } else 
-        return count / wbDescArray[dma.active_channel.number].BTCTRL.bit.BEATSIZE;
-      }
-    }remaining_bytes;   
+typedef struct dma_active_id{
+  inline operator int() { 
+    return DMAC->ACTIVE.bit.ID; 
+  }
+};
 
-  }active_channel;
+typedef struct dma_active_busy{
+  inline operator bool() { 
+    return DMAC->ACTIVE.bit.ABUSY; 
+  }
+};
 
-}dma;
+/////////// DMAC CHANNEL TYPEDEFS ///////////
+
+typedef struct {
+
+
+  bool operator = (void (*dmaErrorCallback)(void)) {
+
+  } 
+
+};
+
+
 
 
 
