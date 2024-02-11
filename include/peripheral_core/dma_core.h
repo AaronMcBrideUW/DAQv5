aa2beaac172c22901ff2f357375d21501ef0b1ca
Aaron425 <151467777+AaronMcBrideUW@users.noreply.github.com>
2024-02-10 22:04:48 -0800

    DMA Refactor in progress

--- include/peripheral_core/dma_core.h
+++ include/peripheral_core/dma_core.h
@@ -10,24 +10,15 @@
 #include "memory.h"
 
 #define IRQ_MAX_PRIORITY 4 /// NOTE: THIS SHOULD BE REPLACED...
-
 #define _super_(_super_, _this_) explicit _this_(_super_ *super):super(super) {}; \
                                 const _super_ *super
-
-enum DMA_ERROR {
-  DMACH_ERROR_NONE,
-  DMACH_ERROR_UNKNOWN,
-  DMACH_ERROR_CRC,
-  DMACH_ERROR_DESC,
-  DMACH_ERROR_TRANSFER
-};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //// SECTION: MISC
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
 /// @typedef DMA callback typedefs
-typedef void (*errorCBType)(int, DMA_ERROR);
+typedef void (*errorCBType)(int, CHANNEL_ERROR);
 typedef void (*transferCBType)(int);
 
 /// @internal DMA storage variables
@@ -35,6 +26,12 @@ DmacDescriptor wbDescArray[DMAC_CH_NUM] __attribute__ ((aligned (16)));
 DmacDescriptor baseDescArray[DMAC_CH_NUM] __attribute__ ((aligned (16))); 
 errorCBType errorCB = nullptr;
 transferCBType transferCB = nullptr;
+
+/// @defgroup DMA Enums
+enum CHANNEL_STATE;
+enum CHANNEL_ERROR;
+enum TRIGGER_SOURCE;
+enum TRIGGER_ACTION;
 
 /// @defgroup DMA hardware properties 
 #define DMA_IRQ_COUNT 5
@@ -55,25 +52,21 @@ const bool PRILVL_ENABLED[DMA_PRILVL_COUNT] = { true };
 
 struct sys{
 
-  bool reset();
-
-  struct init {
-    // TO DO
-  };
+  // More to do -> init() method??
 
   struct enabled{ 
     inline operator bool();
-    bool operator = (const bool value);
+    void operator = (const bool);
   }enabled;
 
   struct errorCallback{
     inline operator errorCBType(); /// MAY HAVE TO ADD BOOL OP HERE...
-    inline bool operator = (const errorCBType errorCallback);
+    inline void operator = (const errorCBType);
   }errorCallback;
 
   struct transferCallback{
     inline operator transferCBType();
-    inline bool operator = (const transferCBType transferCallback);
+    inline void operator = (const transferCBType);
   }transferCallback;
 
 };
@@ -94,12 +87,6 @@ struct activeChannel {
 
 }activeChannel;
 
-enum CHANNEL_STATE {
-  STATE_DISABLED,
-  STATE_IDLE,
-  STATE_ACTIVE,
-  STATE_SUSPENDED
-};
 
 struct channel {
   channel(const int &index):index(index) {};
@@ -107,25 +94,35 @@ struct channel {
 
   bool trigger();
 
+  struct state {
+    bool operator = (const CHANNEL_STATE);
+    inline operator CHANNEL_STATE();
+    inline operator int();
+    inline bool operator == (const CHANNEL_STATE);
+    _super_(channel, state);
+  }state{this};
+
   struct enabled {
     inline operator bool();
-    bool operator = (const bool value);
+    inline void operator = (const bool);
     _super_(channel, enabled);
   }enabled{this};
 
-  struct triggerSrc {
-
-  }triggerSrc;
+  struct triggerSource {
+    inline operator TRIGGER_SOURCE();
+    void operator = (const TRIGGER_SOURCE);
+    _super_(channel, triggerSource);
+  }triggerSrc{this};
 
   struct triggerAction {
+    inline operator TRIGGER_ACTION();
+    void operator = (const TRIGGER_ACTION);
+    _super_(channel, triggerAction);
+  }triggerAction{this};
 
-  }triggerAction;
-
-  struct suspended {
-
-  }suspended;
 
   struct baseDescriptor {
+
 
   }baseDescriptor;
 
@@ -158,6 +155,120 @@ struct channel {
   }config{this};
 
 }channel;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//// SECTION: ENUMS
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+enum CHANNEL_ERROR {
+  ERROR_NONE,
+  ERROR_UNKNOWN,
+  ERROR_CRC,
+  ERROR_DESC,
+  ERROR_TRANSFER
+};
+
+enum CHANNEL_STATE {
+  STATE_DISABLED,
+  STATE_IDLE,
+  STATE_SUSPENDED,
+  STATE_PENDING,
+  STATE_ACTIVE,
+};
+
+enum TRIGGER_ACTION : uint8_t {
+  TRANSFER_BLOCK = 0,
+  TRANSFER_BURST = 2,
+  TRANSFER_ALL   = 3
+};
+
+enum TRIGGER_SOURCE : uint8_t {
+  TRIGGER_SOFTWARE          = 0,
+  TRIGGER_RTC_TIMESTAMP     = 1,
+  TRIGGER_DSU_DCC0          = 2,
+  TRIGGER_DSU_DCC1          = 3,
+  TRIGGER_SERCOM0_RX        = 4,
+  TRIGGER_SERCOM0_TX        = 5,
+  TRIGGER_SERCOM1_RX        = 6,
+  TRIGGER_SERCOM1_TX        = 7,
+  TRIGGER_SERCOM2_RX        = 8,
+  TRIGGER_SERCOM2_TX        = 9,
+  TRIGGER_SERCOM3_RX        = 10,
+  TRIGGER_SERCOM3_TX        = 11,
+  TRIGGER_SERCOM4_RX        = 12,
+  TRIGGER_SERCOM4_TX        = 13,
+  TRIGGER_SERCOM5_RX        = 14,
+  TRIGGER_SERCOM5_TX        = 15,
+  TRIGGER_SERCOM6_RX        = 16,
+  TRIGGER_SERCOM6_TX        = 17,
+  TRIGGER_SERCOM7_RX        = 18,
+  TRIGGER_SERCOM7_TX        = 19,
+  TRIGGER_CAN0_DEBUG_REQ    = 20,
+  TRIGGER_CAN1_DEBUG_REQ    = 21,
+  TRIGGER_TCC0_OOB          = 22,
+  TRIGGER_TCC0_COMPARE_0    = 23,
+  TRIGGER_TCC0_COMPARE_1    = 24,
+  TRIGGER_TCC0_COMPARE_2    = 25,
+  TRIGGER_TCC0_COMPARE_3    = 26,
+  TRIGGER_TCC0_COMPARE_4    = 27,
+  TRIGGER_TCC0_COMPARE_5    = 28,
+  TRIGGER_TCC1_OOB          = 29,
+  TRIGGER_TCC1_COMPARE_0    = 30,
+  TRIGGER_TCC1_COMPARE_1    = 31,
+  TRIGGER_TCC1_COMPARE_2    = 32,
+  TRIGGER_TCC1_COMPARE_3    = 33,
+  TRIGGER_TCC2_OOB          = 34,
+  TRIGGER_TCC2_COMPARE_0    = 35,
+  TRIGGER_TCC2_COMPARE_1    = 36,
+  TRIGGER_TCC2_COMPARE_2    = 37,
+  TRIGGER_TCC3_OOB          = 38,
+  TRIGGER_TCC3_COMPARE_0    = 39,
+  TRIGGER_TCC3_COMPARE_1    = 40,
+  TRIGGER_TCC4_OOB          = 41,
+  TRIGGER_TCC4_COMPARE_0    = 42,
+  TRIGGER_TCC4_COMPARE_1    = 43,
+  TRIGGER_TC0_OOB           = 44,
+  TRIGGER_TC0_COMPARE_0     = 45,
+  TRIGGER_TC0_COMPARE_1     = 46,
+  TRIGGER_TC1_OOB           = 47,
+  TRIGGER_TC1_COMPARE_0     = 48,
+  TRIGGER_TC1_COMPARE_1     = 49,
+  TRIGGER_TC2_OOB           = 50,
+  TRIGGER_TC2_COMPARE_0     = 51,
+  TRIGGER_TC2_COMPARE_1     = 52,
+  TRIGGER_TC3_OOB           = 53,
+  TRIGGER_TC3_COMPARE_0     = 54,
+  TRIGGER_TC3_COMPARE_1     = 55,
+  TRIGGER_TC4_OOB           = 56,
+  TRIGGER_TC4_COMPARE_0     = 57,
+  TRIGGER_TC4_COMPARE_1     = 58,
+  TRIGGER_TC5_OOB           = 59,
+  TRIGGER_TC5_COMPARE_0     = 60,
+  TRIGGER_TC5_COMPARE_1     = 61,
+  TRIGGER_TC6_OOB           = 62,
+  TRIGGER_TC6_COMPARE_0     = 63,
+  TRIGGER_TC6_COMPARE_1     = 64,
+  TRIGGER_TC7_OOB           = 65,
+  TRIGGER_TC7_COMPARE_0     = 66,
+  TRIGGER_TC7_COMPARE_1     = 67,
+  TRIGGER_ADC0_RESRDY       = 68,
+  TRIGGER_ADC0_SEQ          = 69,
+  TRIGGER_ADC1_RESRDY       = 70,
+  TRIGGER_ADC1_SEQ          = 71,
+  TRIGGER_DAC_EMPTY0        = 72,
+  TRIGGER_DAC_EMPTY1        = 73,
+  TRIGGER_DAC_RESULT_READY0 = 74,
+  TRIGGER_DAC_RESULT_READY1 = 75,
+  TRIGGER_I2S_RX0           = 76,
+  TRIGGER_I2S_RX1           = 77,
+  TRIGGER_I2S_TX0           = 78,
+  TRIGGER_I2S_TX1           = 79,
+  TRIGGER_PCC_RX            = 80,
+  TRIGGER_AES_WRITE         = 81,
+  TRIGGER_AES_READ          = 82,
+  TRIGGER_QSPI_RX           = 83,
+  TRIGGER_QSPI_TX           = 84
+};
 
 
 
--- src/peripheral_core/dma_core.cpp
+++ src/peripheral_core/dma_core.cpp
@@ -8,50 +8,39 @@
 //// SECTION: SYSTEM 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
-bool sys::reset() {
-  DMAC->CTRL.bit.DMAENABLE = 0;
-  DMAC->CRCCTRL.reg &= ~DMAC_CRCCTRL_MASK;
-  DMAC->CTRL.bit.SWRST = 1;
-  while(DMAC->CTRL.bit.SWRST);
-
-  memset(wbDescArray, 0, sizeof(wbDescArray));
-  memset(baseDescArray, 0, sizeof(baseDescArray));
-  return true;
-}
-
 sys::enabled::operator bool() {
   return (bool)DMAC->CTRL.bit.DMAENABLE;
 }
+void sys::enabled::operator = (const bool value) {
+  if (value != (bool)DMAC->CTRL.bit.DMAENABLE) {
+    DMAC->CTRL.bit.DMAENABLE = 0;
+    DMAC->CRCCTRL.reg &= ~DMAC_CRCCTRL_MASK;
+    DMAC->CTRL.bit.SWRST = 1;
+    while(DMAC->CTRL.bit.SWRST);
 
-bool sys::enabled::operator = (const bool value) {
-  if (value == (bool)DMAC->CTRL.bit.DMAENABLE) {
-    return true;
-  }
-  if (value) {
-    for (int i = 0; i < DMA_IRQ_COUNT; i++) {
-      NVIC_ClearPendingIRQ((IRQn_Type)(DMAC_0_IRQn + i));
-      NVIC_SetPriority((IRQn_Type)(DMAC_0_IRQn + i), IRQ_PRILVL);
-      NVIC_EnableIRQ((IRQn_Type)(DMAC_0_IRQn + i));
+    memset(wbDescArray, 0, sizeof(wbDescArray));
+    memset(baseDescArray, 0, sizeof(baseDescArray));
+    if (value) {
+      for (int i = 0; i < DMA_IRQ_COUNT; i++) {
+        NVIC_ClearPendingIRQ((IRQn_Type)(DMAC_0_IRQn + i));
+        NVIC_SetPriority((IRQn_Type)(DMAC_0_IRQn + i), IRQ_PRILVL);
+        NVIC_EnableIRQ((IRQn_Type)(DMAC_0_IRQn + i));
+      }
+      for (int i = 0; i < DMA_PRILVL_COUNT; i++) {
+        DMAC->CTRL.reg |= 
+          ((uint8_t)PRILVL_ENABLED[i] << (DMAC_CTRL_LVLEN0_Pos + i));    
+        DMAC->PRICTRL0.reg |= 
+          ((uint8_t)PRILVL_RR_MODE[i] << (DMAC_PRICTRL0_RRLVLEN0_Pos + i * 8))
+        | ((uint8_t)PRILVL_SERVICE_QUAL[i] << (DMAC_PRICTRL0_QOS0_Pos + i * 8));
+      }
+      DMAC->BASEADDR.reg = (uint32_t)baseDescArray;
+      DMAC->WRBADDR.reg = (uint32_t)wbDescArray;
+      DMAC->CTRL.bit.DMAENABLE = 1;
     }
-    for (int i = 0; i < DMA_PRILVL_COUNT; i++) {
-      DMAC->CTRL.reg |= 
-        ((uint8_t)PRILVL_ENABLED[i] << (DMAC_CTRL_LVLEN0_Pos + i));    
-      DMAC->PRICTRL0.reg |= 
-        ((uint8_t)PRILVL_RR_MODE[i] << (DMAC_PRICTRL0_RRLVLEN0_Pos + i * 8))
-      | ((uint8_t)PRILVL_SERVICE_QUAL[i] << (DMAC_PRICTRL0_QOS0_Pos + i * 8));
-    }
-    DMAC->BASEADDR.reg = (uint32_t)baseDescArray;
-    DMAC->WRBADDR.reg = (uint32_t)wbDescArray;
-    DMAC->CTRL.bit.DMAENABLE = 1;
   }
-  return true;
 }
 
-sys::errorCallback::operator errorCBType() {
-  return errorCB;
-}
-
-bool sys::errorCallback::operator = (const errorCBType errorCallback) {
+void sys::errorCallback::operator = (const errorCBType errorCallback) {
   errorCB = errorCallback;
   if (errorCallback) {
     for (int i = 0; i < DMAC_CH_NUM; i++) {
@@ -63,14 +52,12 @@ bool sys::errorCallback::operator = (const errorCBType errorCallback) {
       DMAC->Channel[i].CHINTENCLR.bit.TERR = 1;
     }
   }
-  return true;
+}
+sys::errorCallback::operator errorCBType() {
+  return errorCB;
 }
 
-sys::transferCallback::operator transferCBType() {
-  return transferCB;
-}
-
-bool sys::transferCallback::operator = (const transferCBType transferCallback) {
+void sys::transferCallback::operator = (const transferCBType transferCallback) {
   transferCB = transferCallback; 
   if (transferCallback) {
     for (int i = 0; i < DMAC_CH_NUM; i++) {
@@ -82,7 +69,9 @@ bool sys::transferCallback::operator = (const transferCBType transferCallback) {
       DMAC->Channel[i].CHINTFLAG.bit.TCMPL = 1;
     }
   }
-  return true;
+}
+sys::transferCallback::operator transferCBType() {
+  return transferCB;
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -108,4 +97,95 @@ activeChannel::busy::operator bool() {
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 //// SECTION: DMA CHANNEL 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
+
+void channel::enabled::operator = (const bool value) {
+  DMAC->Channel[super->index].CHCTRLA.bit.ENABLE = (uint8_t)value;
+}
+channel::enabled::operator bool() {
+  return DMAC->Channel[super->index].CHCTRLA.bit.ENABLE;
+}
+
+bool channel::state::operator = (const CHANNEL_STATE value) {
+  auto setSuspend = [&](int value) -> void {
+    if (value > 0) {
+      DMAC->Channel[super->index].CHCTRLB.bit.CMD == DMAC_CHCTRLB_CMD_SUSPEND_Val;
+      while(DMAC->Channel[super->index].CHCTRLB.bit.CMD == DMAC_CHCTRLB_CMD_SUSPEND_Val);
+    } else { 
+      DMAC->Channel[super->index].CHINTFLAG.bit.SUSP = 1;   
+      DMAC->Channel[super->index].CHCTRLB.bit.CMD 
+        = value < 0 ? DMAC_CHCTRLB_CMD_RESUME_Val : DMAC_CHCTRLB_CMD_NOACT_Val;
+    }
+  };
+  auto setEnabled = [&](bool value) -> void {
+    if (value) {
+      DMAC->Channel[super->index].CHCTRLA.bit.ENABLE = 1;
+    } else {
+      setSuspend(0); // May not be necessary...
+      DMAC->Channel[super->index].CHCTRLA.bit.ENABLE = 0;
+      while(DMAC->Channel[super->index].CHSTATUS.bit.BUSY);
+    }
+  };
+  if (*this == value) {
+    return true;
+  } else if (value == STATE_PENDING) {
+    return false;
+  }
+  switch(value) {
+    case STATE_DISABLED: {
+      setEnabled(false);
+    }
+    case STATE_IDLE: {
+      setEnabled(false);
+      setEnabled(true);
+    }
+    case STATE_SUSPENDED: {
+      setEnabled(true);
+      setSuspend(1);
+    }
+    case STATE_ACTIVE: {
+      setEnabled(true);
+      if (DMAC->Channel[super->index].CHINTFLAG.bit.SUSP) { // Ensure not suspended
+        setSuspend(-1);
+      } 
+      if (!DMAC->Channel[super->index].CHSTATUS.bit.BUSY 
+        && DMAC->Channel[super->index].CHSTATUS.bit.PEND) { // If not active (from resume) -> trigger
+        DMAC->SWTRIGCTRL.reg |= (1 << super->index);
+      }
+    }
+  }
+  return true;
+}
+
+//                                                                      NEEDS TO BE REDONE!
+channel::state::operator CHANNEL_STATE() {
+  if (!DMAC->Channel[super->index].CHCTRLA.bit.ENABLE) {
+    return STATE_DISABLED;
+  } else if (DMAC->Channel[super->index].CHINTFLAG.bit.SUSP) {
+    return STATE_SUSPENDED;
+  } else if (DMAC->Channel[super->index].CHSTATUS.bit.BUSY
+    || DMAC->Channel[super->index].CHSTATUS.bit.PEND) {
+    //return STATE_ACTIVE;
+  }
+  return STATE_IDLE;    
+}
+
+void channel::triggerSource::operator = (const TRIGGER_SOURCE value) {
+  DMAC->Channel[super->index].CHCTRLA.bit.TRIGSRC = (uint8_t)value;
+}
+channel::triggerSource::operator TRIGGER_SOURCE() {
+  return static_cast<TRIGGER_SOURCE>
+    (DMAC->Channel[super->index].CHCTRLA.bit.TRIGSRC);
+}
+
+void channel::triggerAction::operator = (const TRIGGER_ACTION value) {
+  DMAC->Channel[super->index].CHCTRLA.bit.TRIGACT = (uint8_t)value;
+}
+channel::triggerAction::operator TRIGGER_ACTION() {
+  return static_cast<TRIGGER_ACTION>
+    (DMAC->Channel[super->index].CHCTRLA.bit.TRIGACT);
+}
+
+
+
+
 
