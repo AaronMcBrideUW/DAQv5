ff1d96421b19ee050cea24eae3c1463cafb0f4ab
Aaron425 <151467777+AaronMcBrideUW@users.noreply.github.com>
2024-02-14 07:29:11 -0800

    Sync (ignore)

--- include/peripheral_core/core_util.h
+++ include/peripheral_core/core_util.h
@@ -6,72 +6,117 @@
 #include <sam.h>
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
-//// SECTION: PROPERTY TYPE DERIVATIVES
+//// SECTION: TEMPLATES 
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 
+template<typename T, typename mod = void>
+class Property {};
 
-template<class T, bool id, class Enable = void>
-class Property {
-  constexpr Property(...) {
-    static_assert(true, "Property is ill-formed: Invalid type");
-  }
+template<typename T>
+struct _isBasic {
+  static const bool result 
+    = std::is_integral<T>::value 
+    && !std::is_pointer<T>::value
+    && !std::is_same<T, uintptr_t>::value;
 };
 
-template<class T, bool id>  
-class Property<T, false, typename std::enable_if<std::is_integral<T>::value>::type> {
-    constexpr Property(bool (*set)(const T&), T (*get)(void), int *regValue) {}
-      : set(set), get(get) {};
+template<typename T>
+class Property<T, typename std::conditional<_isBasic<T>::result, T, void>::type> {
+  public:
+    Property(bool (&set)(const T&), T (&get)(void)) : set(set), get(get) {};
 
-    operator T() const {
-      return get();
-    }
     bool operator = (const T &value) {
       return set(value);
     }
-  protected:
-    bool (*set)(const T&, const int&);
-    T (*get)(const int*);
-};
-
-template<class T, bool id>  
-class Property<T, false, typename std::enable_if<std::is_enum<T>::value>::type> {
-    constexpr Property(bool (*set)(const T&), T (*get)(void)) {}
-      : set(set), get(get) {};
-
-    operator T() const {
+    operator T() {
       return get();
     }
-    operator std::underlying_type<T>::type() const {
-      return static_cast<std::underlying_type<T>::type>(get());
-    }
-    bool operator = (const T &value) {
-      return set(value);
-    }
-    bool operator = (const std::underlying_type<T>::type &value) {
-      return set(static_cast<const T>(value));
-    }
-    operator == (const T &value) {
-      return value == get();
-    }
-
-  protected:
-    bool (*set)(const T&);
-    T (*get)(void);
+  private:
+    bool (&set)(const T&);
+    T (&get)(void);
 };
 
-template<class T, bool id>
-class Property<T, true, typename std::enable_if<std::is_integral<T>::value>::type> {
-  public: 
-    constexpr Property(bool (*set)(const T&), T (*get)(void), int *regValue) {}
-      : set(set), get(get) {};
+template<typename T>
+struct _isEnum {
+  static const bool result 
+    = std::is_enum<T>::value 
+    && !std::is_pointer<T>::value
+    && !std::is_same<T, uintptr_t>::value;
+};
 
-    operator T() const {
+template<typename T>
+class Property<T, typename std::conditional<_isEnum<T>::result, T, void>::type> {
+  public:
+    Property(bool (&set)(const T&), T (&get)(void)) : set(set), get(get) {}; 
+
+    bool operator = (const T &value) {
+      d
+    }
+    bool operator = (const typename std::underlying_type<T>::type &value) {
+      return set((T)value);
+    }
+    operator T() {
       return get();
     }
-    bool operator = (const T &value) {
+    operator typename std::underlying_type<T>::type() {
+      return (typename std::underlying_type<T>::type)get();
+    }
+    bool operator == (const T &value) {
+      return (value == get());
+    }
+  private:
+    bool (&set)(const T&);
+    T (&get)(void);
+};
+
+template<typename T>
+struct _isPtr {
+  static const bool result 
+    = std::is_pointer<T>::value
+    && std::is_same<t, uintptr_t>::value
+    && !std::is_class<T>::value;
+};
+
+/// @brief For addresses
+template<typename T>
+class Property<T, typename std::conditional<_isPtr<T>::type, T, void>::type> {
+  public:
+    Property(bool (&set)(T), T (&get)(void)) : set(set), get(get) {}; 
+
+    bool operator = (T value) {
       return set(value);
     }
-  protected:
-    bool (*set)(const T&, const int&);
-    T (*get)(const int*);
-}
+    bool operator = (const uintptr_t &value) {
+      return set((T)value);
+    }
+    operator T() {
+      return get();
+    }
+    operator uintptr_t() {
+      return (uintptr_t)get();
+    }
+    operator bool() {
+      return get() != nullptr;
+    }
+    bool operator == (const T value) {
+      return value = get(); 
+    }
+    T operator ++ (&this) {
+      set(get()--);
+      return get();
+    }
+    T operator -- (&this) {
+      set(get()--);
+    }
+  
+  private:
+    bool (&set)(const T&);
+    T (&get)(void);
+};
+
+template<typename T>
+class Property<T, typename std::enable_if<std::is_array<T>::value>::type> {
+  private:
+    bool (&set)(const T&);
+    T (&get)(void);
+};
